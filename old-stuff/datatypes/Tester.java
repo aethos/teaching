44f35f9869ec17bf92f7289b6a4e87d107b25339
Ethan Adams <ea2678@columbia.edu>
2016-10-13 14:27:37 -0400

    moving and clarifying

---
+++ old-stuff/datatypes/Tester.java
@@ -0,0 +1,31 @@
+/**
+Tester for datatypes
+What works, and what doesn't?
+*/
+
+public class Tester
+{
+	public static void main(String[] args)
+	{
+		System.out.println(2 == 2.0);
+
+		// System.out.println(2 == "2");
+
+		System.out.println(String.valueOf('2') == "2");
+
+		System.out.println("2.0" == "2");
+
+		String first = "2";
+		String second = "21";
+		second = second.substring(0, 1);
+		// now second is also "2"
+
+		System.out.println(first == second);
+
+		System.out.println("2".equals("2"));
+
+		// System.out.println((Integer.parseInt("2") == Integer.parseInt("2.0")));
+
+		System.out.println('2' == 50);
+	}
+}
